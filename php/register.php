90826282feb16a374a57e798f12fef879231efc8
Eduardo <51763596+EduNSae@users.noreply.github.com>
2020-10-10 12:43:07 -0300

    Fazendo o remodelação completa
    Organizando os arquivos, fazendo breve atualizações nos arquivos ja existentes e inserindo a parte de produtos no projeto.

---
+++ php/register.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+include("connect.php");
+
+$nomeComplete = filter_input(INPUT_POST, 'nome');
+$endereco = $_POST['endereco'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+$date = $_POST['date'];
+
+$sql = "INSERT INTO usuario (nome, email, senha, endereco,dateCriacao,dateNasc)
+         VALUES ('$nomeComplete','$email', '$senha', '$endereco', CURDATE(), '$date')";
+    
+    if ($link->query($sql) === TRUE) {
+        echo "<script>
+                alert('cadastrado com sucesso');
+                window.location.href = '../login.html';
+              </script>";
+            
+    } else {
+        echo "<script>
+                alert('Dados inseridos incorretamente');
+                 window.location.href = '../signup.html';
+              </script>";
+    }
+
+exit;
+?>
